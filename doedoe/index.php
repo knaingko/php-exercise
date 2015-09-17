2254874d95ce13f651d9c67b213f7c95a7371cd6
Kyaw Naing <knaingko@gmail.com>
2015-09-17 23:48:55 +0630

    Initial commit

---
+++ doedoe/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Knaingko-PC
+ * Date: 9/17/2015
+ * Time: 4:36 PM
+ */
---
+++ kn/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Knaingko-PC
+ * Date: 9/16/2015
+ * Time: 9:09 PM
+ */
+echo '<a href="datalist.php">doedoe</a>';
