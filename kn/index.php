180e290860c9cd670111ff6eaf28120761c2311b
Kyaw Naing <knaingko@gmail.com>
2015-09-20 01:33:16 +0630

    Fix the menu

--- kn/index.php
+++ kn/index.php
@@ -5,7 +5,6 @@
  * Date: 9/16/2015
  * Time: 9:09 PM
  */
-echo '<a href="datalist.php">doedoe</a>';
-echo '<a href="datalist.php">doedoe</a>';
-echo '<a href="datalist.php">doedoe</a>';
-echo '<a href="datalist.php">doedoe</a>';
+echo '<a href="./doedoe/datalist.php">doedoe</a>';
+echo '<a href="./mmt/datalist.php">mmt</a>';
+echo '<a href="./kmn/datalist.php">kmn</a>';
