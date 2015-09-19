a11936b22f2cbda3bf8c4476be9d15de20254c94
Kyaw Naing <knaingko@gmail.com>
2015-09-19 21:48:34 +0630

    menu add

--- kn/index.php
+++ kn/index.php
@@ -6,3 +6,6 @@
  * Time: 9:09 PM
  */
 echo '<a href="datalist.php">doedoe</a>';
+echo '<a href="datalist.php">doedoe</a>';
+echo '<a href="datalist.php">doedoe</a>';
+echo '<a href="datalist.php">doedoe</a>';
